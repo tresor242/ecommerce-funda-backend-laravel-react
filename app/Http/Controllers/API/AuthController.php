d19aac51234f8dcc69cc7927bb77c9ca1b6eef7b
Tresor Elila <elilatresor25@gmail.com>
2025-06-09 01:25:33 -0400

    Mise à jour du système d’authentification en backend

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -42,4 +42,51 @@ class AuthController extends Controller
             ]);
         }
     }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|max:191',
+            'password'=>'required',
+        ]);
+
+        if ($validator->fails()) 
+        {
+            return response()->json([
+                'validation_errors'=>$validator->messages(),
+            ]);
+        }
+        else
+        {
+            $user = User::where('email', $request->email)->first();
+ 
+            if(! $user || ! Hash::check($request->password, $user->password)) 
+            {
+                return response()->json([
+                    'status'=>401,
+                    'message' => 'Invalid Credentials',
+                ]);
+            }
+            else
+            {
+                $token = $user->createToken($user->email.'_Token')->plainTextToken;
+
+                return response()->json([
+                    'status'=>200,
+                    'username' => $user->name,
+                    'token'=>$token,
+                    'message' => 'Logged In successfully',
+                ]);
+            }
+        }
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            'status'=>200,
+            'message' => 'Logged Out Successfully !',
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
